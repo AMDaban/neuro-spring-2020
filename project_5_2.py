f35ef8e3e39077051458569772098e260107e197
Alireza Mohammadi <daban.am56@gmail.com>
2020-07-06 22:07:55 +0430

    complete phase5-2

--- project_5_2.py
+++ project_5_2.py
@@ -5,43 +5,37 @@ import matplotlib.pyplot as plt
 from neurokit.filters.gabor import Gabor2D
 from neurokit.operators.convoloution import Convolution2D
 
-IMAGE_PATH = "pics/texture.jpg"
-
-GABOR_SIZE = 37
-GABOR_LAMBDA = 10
-GABOR_THETA = 1/2 * np.pi 
-GABOR_SIGMA = 3
-GABOR_GAMMA = 0.5
-GABOR_PARTITIONS = 5
-
-CONVOLUTION_PADDING = True
-CONVOLUTION_STRIDE = 1
-CONVOLUTION_KEEPING_PERCENTAGE = 0.2
+IMAGE_PATH = "pics/car.jpg"
 
 def main():
+    main_single(g_size=17,g_lambda=15,g_sigma=7.5,g_gamma=0.5,g_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0)
+
+def main_single(g_size, g_lambda, g_sigma, g_gamma, g_partitions, conv_padding, conv_stride, conv_kp):
     img = cv2.imread(IMAGE_PATH, 0) 
 
     plt.imshow(img, 'gray')
     plt.show()
 
-    kernel = Gabor2D(GABOR_SIZE, GABOR_LAMBDA, GABOR_THETA, GABOR_SIGMA, GABOR_GAMMA).compute()
-    operator = Convolution2D(kernel, stride=CONVOLUTION_STRIDE, pad_inp=CONVOLUTION_PADDING)
+    for i in range(4):
+        theta = i * (np.pi)/4
+        kernel = Gabor2D(g_size, g_lambda, theta, g_sigma, g_gamma).compute()
+        operator = Convolution2D(kernel, stride=conv_stride, pad_inp=conv_padding)
 
-    result = operator.apply(img)
-    result = normalize_image(result)
-    result = result * (result >= CONVOLUTION_KEEPING_PERCENTAGE * np.max(result))
+        result = operator.apply(img)
+        result = normalize_image(result)
+        result = result * (result >= conv_kp * np.max(result))
 
-    plt.imshow(result, 'gray')
-    plt.show()
-
-    gabor_partition_size = 255 / GABOR_PARTITIONS
-    for partition_idx in range(GABOR_PARTITIONS):
-        lower_bound = partition_idx * gabor_partition_size
-        upper_bound = (partition_idx + 1) * gabor_partition_size
-        partition = result * (lower_bound <= result)
-        partition = partition * (partition < upper_bound)
-        plt.imshow(partition, 'gray')
+        plt.imshow(result, 'gray')
         plt.show()
+
+        gabor_partition_size = 255 / g_partitions
+        for partition_idx in range(g_partitions):
+            lower_bound = partition_idx * gabor_partition_size
+            upper_bound = (partition_idx + 1) * gabor_partition_size
+            partition = result * (lower_bound <= result)
+            partition = partition * (partition < upper_bound)
+            plt.imshow(partition, 'gray')
+            plt.show()
 
 def normalize_image(image):
     unit = (np.max(image) - np.min(image)) / 255
