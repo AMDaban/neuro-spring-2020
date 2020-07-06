7fdecd96496bd71b0e078fc594479267790b484b
Alireza Mohammadi <daban.am56@gmail.com>
2020-07-06 20:44:23 +0430

    Complete phase5-1

--- project_5_1.py
+++ project_5_1.py
@@ -5,35 +5,33 @@ import matplotlib.pyplot as plt
 from neurokit.filters.dog import DOG2D
 from neurokit.operators.convoloution import Convolution2D
 
-IMAGE_PATH = "pics/pic.jpg"
-
-DOG_SIZE = 7
-DOG_SIGMA_1 = 0.3
-DOG_SIGMA_2 = 1
-DOG_PARTITIONS = 3
-
-CONVOLUTION_PADDING = True
-CONVOLUTION_STRIDE = 1
-CONVOLUTION_KEEPING_PERCENTAGE = 0.2
+IMAGE_PATH = "pics/car.jpg"
 
 def main():
+    main_single(dog_size=7,dog_sigma_1=0.3,dog_sigma_2=1,dog_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0.2)
+    main_single(dog_size=11,dog_sigma_1=0.3,dog_sigma_2=1,dog_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0.2)
+    main_single(dog_size=15,dog_sigma_1=0.3,dog_sigma_2=1,dog_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0.2)
+    main_single(dog_size=19,dog_sigma_1=0.3,dog_sigma_2=1,dog_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0.2)
+    main_single(dog_size=23,dog_sigma_1=0.3,dog_sigma_2=1,dog_partitions=3,conv_padding=True,conv_stride=1,conv_kp=0.2)
+
+def main_single(dog_size, dog_sigma_1, dog_sigma_2, dog_partitions, conv_padding, conv_stride, conv_kp):
     img = cv2.imread(IMAGE_PATH, 0)
 
     plt.imshow(img, 'gray')
     plt.show()
 
-    kernel = DOG2D(DOG_SIZE, DOG_SIGMA_1, DOG_SIGMA_2).compute()
-    operator = Convolution2D(kernel, stride=CONVOLUTION_STRIDE, pad_inp=CONVOLUTION_PADDING)
+    kernel = DOG2D(dog_size, dog_sigma_1, dog_sigma_2).compute()
+    operator = Convolution2D(kernel, stride=conv_stride, pad_inp=conv_padding)
 
     result = operator.apply(img)
     result = normalize_image(result)
-    result = result * (result >= CONVOLUTION_KEEPING_PERCENTAGE * np.max(result))
+    result = result * (result >= conv_kp * np.max(result))
 
     plt.imshow(result, 'gray')
     plt.show()
 
-    dog_partition_size = 255 / DOG_PARTITIONS
-    for partition_idx in range(DOG_PARTITIONS):
+    dog_partition_size = 255 / dog_partitions
+    for partition_idx in range(dog_partitions):
         lower_bound = partition_idx * dog_partition_size
         upper_bound = (partition_idx + 1) * dog_partition_size
         partition = result * (lower_bound <= result)
